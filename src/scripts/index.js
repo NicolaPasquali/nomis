c5dad078e241859348002d14d5c483f3adccfa77
Nicola Pasquali <nicola_pasquali@outlook.it>
2019-08-13 22:54:52 +0200

    Chosen name and drew buttons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,31 +1,42 @@
 /** @author Nicola Pasquali */
-// import '../styles/index.scss';
+import '../styles/index.scss';
 import { init, Sprite, GameLoop } from 'kontra';
+const { canvas } = init();
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+const COLORS = [
+	{ normal: '#2196f2', pressed: '#03a9f3' },
+	{ normal: '#4caf50', pressed: '#8bc24a' },
+	{ normal: '#fe9800', pressed: '#fec007' },
+	{ normal: '#673ab7', pressed: '#9c27b0' }
+];
+let sprites = [];
 
-let { canvas } = init();
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 
-let sprite = Sprite({
-  x: 100,        // starting x,y position of the sprite
-  y: 80,
-  color: 'red',  // fill color of the sprite rectangle
-  width: 20,     // width and height of the sprite rectangle
-  height: 40,
-  dx: 2          // move the sprite 2px to the right every frame
+// The magic number is the sum of the lateral and inner spacing of the buttons
+const buttonSize = (WIDTH - 30) / 2;
+
+for (let row = 0; row < 2; row++) {
+	for (let column = 0; column < 2; column++) {
+		sprites.push(
+			Sprite({
+				x: 10 + (row * buttonSize + row * 10),
+				y: 10 + (column * buttonSize + column * 10),
+				color: COLORS[+`0b${row}${column}`].normal, // We're converting the two indexes into a base two number to access the color
+				width: buttonSize,
+				height: buttonSize
+			})
+		);
+	}
+}
+
+const gameLoop = GameLoop({
+	update: () => {},
+	render: () => {
+		sprites.forEach(sprite => sprite.render());
+	}
 });
 
-let loop = GameLoop({  // create the main game loop
-  update: function() { // update the game state
-    sprite.update();
-
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width;
-    }
-  },
-  render: function() { // render the game state
-    sprite.render();
-  }
-});
-
-loop.start();    // start the game
+gameLoop.start();
