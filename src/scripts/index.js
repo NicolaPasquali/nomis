2867ebd7aab3e711e6883f50409a70a7f3542a35
Nicola Pasquali <nicola_pasquali@outlook.it>
2019-08-15 16:24:22 +0200

    The buttons are now clickable

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 /** @author Nicola Pasquali */
 import '../styles/index.scss';
-import { init, Sprite, GameLoop } from 'kontra';
+import { init, initPointer, track, Sprite, GameLoop } from 'kontra';
 const { canvas } = init();
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
@@ -8,10 +8,11 @@ const COLORS = [
 	{ normal: '#2196f2', pressed: '#03a9f3' },
 	{ normal: '#4caf50', pressed: '#8bc24a' },
 	{ normal: '#fe9800', pressed: '#fec007' },
-	{ normal: '#673ab7', pressed: '#9c27b0' }
+	{ normal: '#d50000', pressed: '#ff1744' }
 ];
 let sprites = [];
 
+initPointer();
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
@@ -20,15 +21,23 @@ const buttonSize = (WIDTH - 30) / 2;
 
 for (let row = 0; row < 2; row++) {
 	for (let column = 0; column < 2; column++) {
-		sprites.push(
-			Sprite({
-				x: 10 + (row * buttonSize + row * 10),
-				y: 10 + (column * buttonSize + column * 10),
-				color: COLORS[+`0b${row}${column}`].normal, // We're converting the two indexes into a base two number to access the color
-				width: buttonSize,
-				height: buttonSize
-			})
-		);
+		const button = Sprite({
+			x: 10 + (row * buttonSize + row * 10),
+			y: 10 + (column * buttonSize + column * 10),
+			color: COLORS[+`0b${row}${column}`].normal,
+			width: buttonSize,
+			height: buttonSize,
+			normalColor: COLORS[+`0b${row}${column}`].normal,
+			pressedColor: COLORS[+`0b${row}${column}`].pressed,
+			onDown: function() {
+				this.color = this.pressedColor;
+			},
+			onUp: function() {
+				this.color = this.normalColor;
+			}
+		});
+		sprites.push(button);
+		track(button);
 	}
 }
 
